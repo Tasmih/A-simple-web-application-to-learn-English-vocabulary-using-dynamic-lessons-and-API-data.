8188a5e4ba42b757e2a300d58a89bb9d41cf13d0
Tasmih <tasmih.mow20@gmail.com>
2026-03-05 11:42:52 +0600

    Fix API data loading issue in JavaScript and Handle empty data state when no words are found

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -100,3 +100,24 @@ const displayLevelWord = (words) =>{
     });
     manageSpinner(false);
 };
+displayLesson=(lessons)=>{
+   //1.get the container & empty
+
+   const levelContainer =document.getElementById("level-container");
+   levelContainer.innerHTML="";
+   //2.get into every lessons
+   for (let lesson of lessons){ 
+    console.log(lesson);  
+    //3. create Element
+    const btnDiv= document.createElement("div");
+    btnDiv.innerHTML = `
+     <button id="lesson-btn-${lesson.level_no}"onclick="loadLevelWord(${lesson.level_no})"class="btn btn-outline btn-primary lesson-btn">
+         <i class="fa-solid fa-book-open"></i>Lesson -${lesson.level_no}
+     </button>
+    `;
+   //4. append into container
+   levelContainer.append(btnDiv);
+   }
+
+};
+ loadLesson();
